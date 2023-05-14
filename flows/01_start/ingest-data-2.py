270c36357575d4467afac8ce0b4dc8d52010571a
JM Reguyal <josemarireguyal@gmail.com>
2023-05-14 20:23:02 +0800

    zoomcamp added

---
+++ flows/01_start/ingest-data-2.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import argparse
+
+from time import time
+
+import pandas as pd
+from sqlalchemy import create_engine
+
+def main(params):
+    user = params.user
+    password = params.password
+    host = params.host
+    port = params.port
+    db = params.db
+    table_name = params.table_name
+    url = params.url
+
+    # the backup files are gzipped, and it's important to keep the correct extension
+    # for pandas to be able to open the file
+    if url.endswith('.csv.gz'):
+        csv_name = 'output.csv.gz'
+    else:
+        csv_name = 'output.csv'
+    
+    os.system(f"wget {url} -O {csv_name}")
+
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+
+    df_iter = pd.read_csv(csv_name, iterator = True, chunksize=100000)
+
+    df = next(df_iter)
+
+    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
+    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+
+    df.to_sql(name=table_name, con=engine, if_exists='append')
+
+    while True:
+        t_start = time()
+        
+        df = next(df_iter)
+        
+        df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+        df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+        
+        df.to_sql(name=table_name, con=engine, if_exists='append')
+        
+        t_end = time()
+        
+        print('Inserted another chunk, took %.3f second' % (t_end - t_start))
+
+
+if __name__ == '__main__':
+    user = "root"
+    password = "root"
+    host = "localhost"
+    port = "5432"
+    db = "ny_taxi"
+    table_name = "yellow_taxi_data"
+    url = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz"
+
+
+
+
+
+
+
+
